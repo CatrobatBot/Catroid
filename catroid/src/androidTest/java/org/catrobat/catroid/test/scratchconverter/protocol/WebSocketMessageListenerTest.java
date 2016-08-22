83981015ca42ae50bc92937b522a6b6ddbb5f6f8
CatrobatBot <noreply@catrobat.org>
2016-08-22 13:58:23 +0200

    Reformat PR#1889

--- catroid/src/androidTest/java/org/catrobat/catroid/test/scratchconverter/protocol/WebSocketMessageListenerTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/test/scratchconverter/protocol/WebSocketMessageListenerTest.java
@@ -55,7 +55,6 @@ import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
-import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
