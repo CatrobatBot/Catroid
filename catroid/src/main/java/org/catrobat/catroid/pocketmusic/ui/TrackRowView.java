94c036af51d5da4b3e0f3884c0b0ce3f8992a1b0
CatrobatBot <noreply@catrobat.org>
2016-10-01 15:09:38 +0200

    Reformat PR#1948

--- catroid/src/main/java/org/catrobat/catroid/pocketmusic/ui/TrackRowView.java
+++ catroid/src/main/java/org/catrobat/catroid/pocketmusic/ui/TrackRowView.java
@@ -29,7 +29,6 @@ import android.widget.TableRow;
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.pocketmusic.note.MusicalBeat;
 import org.catrobat.catroid.pocketmusic.note.trackgrid.GridRow;
-
 import org.catrobat.catroid.pocketmusic.note.trackgrid.GridRowPosition;
 
 import java.math.BigDecimal;
