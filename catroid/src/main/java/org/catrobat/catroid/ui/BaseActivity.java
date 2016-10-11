490d2567405708de5db862cb83aa509866d7be02
CatrobatBot <noreply@catrobat.org>
2016-10-11 14:59:03 +0200

    Reformat PR#1959

--- catroid/src/main/java/org/catrobat/catroid/ui/BaseActivity.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/BaseActivity.java
@@ -27,25 +27,18 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 
-import org.catrobat.catroid.BuildConfig;
 import org.catrobat.catroid.ProjectManager;
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.ui.dialogs.AboutDialogFragment;
 import org.catrobat.catroid.ui.dialogs.TermsOfUseDialogFragment;
 import org.catrobat.catroid.utils.ToastUtil;
 import org.catrobat.catroid.utils.Utils;
-
-import java.util.Locale;
 
 public abstract class BaseActivity extends Activity {
 
