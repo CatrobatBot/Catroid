ebceac685d8c70de4337f27224c44dd128aa5d7c
CatrobatBot <noreply@catrobat.org>
2016-10-19 18:25:15 +0200

    Reformat PR#1974

--- catroid/src/main/java/org/catrobat/catroid/content/bricks/SetLookBrick.java
+++ catroid/src/main/java/org/catrobat/catroid/content/bricks/SetLookBrick.java
@@ -183,7 +183,7 @@ public class SetLookBrick extends BrickBaseType implements OnLookDataListChanged
 	@Override
 	public List<SequenceAction> addActionToSequence(Sprite sprite, SequenceAction sequence) {
 		sequence.addAction(sprite.getActionFactory().createSetLookAction(getSprite(), look, wait));
-		
+
 		return null;
 	}
 
