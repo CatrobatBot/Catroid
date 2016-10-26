e9f9f4d79c28b99f5f2e4473f28a846c0f3ce1b3
Robert Painsi <robertpainsi@gmail.com>
2016-10-27 00:20:18 +0200

    CAT-REF Remove paused member from Script and Sprite
    The paused member was used in the old implementation when we handled the
    action execution by threads. A long time ago we switched to libGDX
    actions. So there is no need for the member anymore.
    
    The only case where the member is used, is in Look.doTouchDown [1].
    However, when pausing the stage, a grey overlay covers the whole stage.
    All touch events are captured by this overlay and no stage touch events
    are fired.
    
    It's important that no touch events are fired when the stage is paused
    for Sprites and TouchUtil.
    
    [1] https://github.com/Catrobat/Catroid/blob/2ce3242fda5f8c020757a9e31fd6d78f111822c5/catroid/src/main/java/org/catrobat/catroid/content/Look.java#L164-L166

---
+++ catroid/src/androidTest/java/org/catrobat/catroid/test/content/sprite/StartScriptTest.java
@@ -0,0 +1,57 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2016 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.test.content.sprite;
+
+import android.test.AndroidTestCase;
+
+import org.catrobat.catroid.content.Script;
+import org.catrobat.catroid.content.SingleSprite;
+import org.catrobat.catroid.content.Sprite;
+import org.catrobat.catroid.content.StartScript;
+import org.catrobat.catroid.content.bricks.HideBrick;
+import org.catrobat.catroid.content.bricks.SetSizeToBrick;
+import org.catrobat.catroid.content.bricks.ShowBrick;
+import org.catrobat.catroid.content.bricks.WaitBrick;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class StartScriptTest extends AndroidTestCase {
+
+	public void testStartScript() throws InterruptedException {
+		float size = 300;
+		Sprite sprite = new SingleSprite("testSprite");
+		Script startScript = new StartScript();
+		startScript.addBrick(new HideBrick());
+		startScript.addBrick(new SetSizeToBrick(size));
+		sprite.addScript(startScript);
+
+		sprite.createStartScriptActionSequenceAndPutToMap(new HashMap<String, List<String>>());
+		while (!sprite.look.getAllActionsAreFinished()) {
+			sprite.look.act(1.0f);
+		}
+
+		assertFalse("Look is not hidden", sprite.look.isLookVisible());
+		assertEquals("the size is not as expected", size, sprite.look.getSizeInUserInterfaceDimensionUnit());
+	}
+}
