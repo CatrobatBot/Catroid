e5cdef01e0501c7a7ff3f114d6a7e2beb03bdf09
Thomas Schranz <thomas.schranz@student.tugraz.at>
2016-11-03 20:42:01 +0100

    Adapted ProjectListFragment and adapter

---
+++ catroid/src/main/java/org/catrobat/catroid/ui/adapter/ProjectListAdapter.java
@@ -0,0 +1,60 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2016 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.ui.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.catrobat.catroid.common.ProjectData;
+import org.catrobat.catroid.io.ProjectAndSceneScreenshotLoader;
+import org.catrobat.catroid.io.StorageHandler;
+
+import java.util.List;
+
+public class ProjectListAdapter extends CheckBoxListAdapter<ProjectData> {
+
+	public static final String TAG = ProjectListAdapter.class.getSimpleName();
+
+	ProjectAndSceneScreenshotLoader screenshotLoader;
+
+	public ProjectListAdapter(Context context, int resource, List<ProjectData> listItems) {
+		super(context, resource, listItems);
+		screenshotLoader = new ProjectAndSceneScreenshotLoader(context);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View listItemView = super.getView(position, convertView, parent);
+
+		ListItemViewHolder listItemViewHolder = (ListItemViewHolder) listItemView.getTag();
+		ProjectData projectData = getItem(position);
+
+		listItemViewHolder.name.setText(projectData.projectName);
+		String sceneName = StorageHandler.getInstance().getFirstSceneName(projectData.projectName);
+
+		screenshotLoader.loadAndShowScreenshot(projectData.projectName, sceneName, false, listItemViewHolder.image);
+
+		return listItemView;
+	}
+}
