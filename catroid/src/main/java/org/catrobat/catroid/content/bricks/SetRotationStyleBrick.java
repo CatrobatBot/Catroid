41ac85215bb0b12adfee57db73f1511fdae9b2a7
CatrobatBot <noreply@catrobat.org>
2016-11-17 17:20:37 +0100

    Reformat PR#2009

--- catroid/src/main/java/org/catrobat/catroid/content/bricks/SetRotationStyleBrick.java
+++ catroid/src/main/java/org/catrobat/catroid/content/bricks/SetRotationStyleBrick.java
@@ -37,7 +37,6 @@ import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.content.Sprite;
-import org.catrobat.catroid.formulaeditor.Formula;
 
 import java.util.List;
 
