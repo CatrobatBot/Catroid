1eafe7ae29a323086b2956a91e7ec299337ce180
CatrobatBot <noreply@catrobat.org>
2016-11-24 00:40:41 +0100

    Reformat PR#2024

--- catroid/src/main/java/org/catrobat/catroid/content/bricks/DroneTurnLeftMagnetoBrick.java
+++ catroid/src/main/java/org/catrobat/catroid/content/bricks/DroneTurnLeftMagnetoBrick.java
@@ -30,7 +30,6 @@ import android.widget.TextView;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
 import org.catrobat.catroid.R;
-import org.catrobat.catroid.content.Scene;
 import org.catrobat.catroid.content.Sprite;
 import org.catrobat.catroid.formulaeditor.Formula;
 
--- catroid/src/main/java/org/catrobat/catroid/content/bricks/SpeakAndWaitBrick.java
+++ catroid/src/main/java/org/catrobat/catroid/content/bricks/SpeakAndWaitBrick.java
@@ -31,7 +31,6 @@ import android.widget.TextView;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
 import org.catrobat.catroid.R;
-import org.catrobat.catroid.content.Scene;
 import org.catrobat.catroid.content.Sprite;
 import org.catrobat.catroid.content.actions.SpeakAction;
 import org.catrobat.catroid.formulaeditor.Formula;
