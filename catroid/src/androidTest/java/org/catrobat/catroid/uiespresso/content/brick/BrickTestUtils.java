4b6cdc54e27d13d16458826ac288047c5a1856b5
CatrobatBot <noreply@catrobat.org>
2017-02-22 15:26:07 +0100

    Reformat PR#2119

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/BrickTestUtils.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/BrickTestUtils.java
@@ -45,7 +45,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.Matchers.instanceOf;
 
-public final class  BrickTestUtils {
+public final class BrickTestUtils {
 	private BrickTestUtils() {
 		throw new AssertionError();
 	}
